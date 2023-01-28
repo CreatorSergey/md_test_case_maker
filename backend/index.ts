faaa4c7517c40ee0c10eb87dfc7cb1776826177c
Creator <bydarkcreator@gmail.com>
2023-01-28 15:09:08 +0500

    prepare express

--- backend/index.ts
+++ backend/index.ts
@@ -1,31 +1,72 @@
-import Mustache = require('mustache');
-import fs = require('fs');
+import Mustache = require("mustache");
+import fs = require("fs");
+import express from "express";
 
-interface ITestCase {
-  id: number
-  title: string,
-  author: string
+/**
+ * Инстансы
+ */
+const app = express();
+
+/**
+ * Константы
+ */
+const temp_folder: string = "./temp";
+const port: number = 3000;
+
+/**
+ * Подготовка папки
+ * @param folder - папка
+ */
+function prepare_folder(folder: string): void {
+  try {
+    fs.mkdirSync(folder);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+/**
+ * Тест кейс
+ */
+interface ITestCase {
+  id: number;
+  title: string;
+  author: string;
+}
+
+/**
+ * Экземпляр кейса
+ */
 let test_case: ITestCase = {
   id: 1,
-  title: 'Создание проекта',
-  author: 'Jonh Smith'
+  title: "Создание проекта",
+  author: "Jonh Smith",
+};
+
+/**
+ * Сохранить файл
+ */
+function save_file(test_case: ITestCase, path: string): string {
+  const output = Mustache.render("# {{title}}\r\n" + "## {{author}}", test_case);
+
+  console.log(output);
+
+  fs.writeFileSync(path, output);
+  return output;
 }
 
-const output = Mustache.render(
-  "# {{title}}\r\n" +
-  "## {{author}}", test_case);
+/**
+ * Связывание
+ */
+app.get("/save_file", (request, result) => {
+  prepare_folder(temp_folder);
+  let text = save_file(test_case, temp_folder + "/" + test_case.id + ".md");
+  result.send(text);
+});
 
-console.log(output);
-
-const temp_folder: string = './temp';
-
-try {
-  fs.mkdirSync(temp_folder);
-}
-catch (e) {
-  console.log(e);
-}
-
-fs.writeFileSync(temp_folder + '/' + test_case.id + ".md  ", output);
+/**
+ * Запуск
+ */
+app.listen(port, () => {
+  console.log(`Серер запущен, порт ${port}`);
+});
