c27b20665772a902e146ec23e5b04327d1aa827c
Creator <bydarkcreator@gmail.com>
2023-01-28 14:22:04 +0500

    prepare

--- backend/index.ts
+++ backend/index.ts
@@ -1,2 +1,31 @@
-let message: string = 'Hello World 2';
-console.log(message)
+import Mustache = require('mustache');
+import fs = require('fs');
+
+interface ITestCase {
+  id: number
+  title: string,
+  author: string
+}
+
+let test_case: ITestCase = {
+  id: 1,
+  title: 'Создание проекта',
+  author: 'Jonh Smith'
+}
+
+const output = Mustache.render(
+  "# {{title}}\r\n" +
+  "## {{author}}", test_case);
+
+console.log(output);
+
+const temp_folder: string = './temp';
+
+try {
+  fs.mkdirSync(temp_folder);
+}
+catch (e) {
+  console.log(e);
+}
+
+fs.writeFileSync(temp_folder + '/' + test_case.id + ".md  ", output);
--- backend/install.sh
+++ backend/install.sh
@@ -1,2 +1,4 @@
 npm install -g typescript
 tsc --version
+npm install mustache --save
+npm install --save @types/mustache
